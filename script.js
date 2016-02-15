104ede335f59fe8948970a25293dcb78d6aa6d1c
Aleksa <rakicaleksa@ymail.com>
2016-02-15 20:21:20 +0100

    loop over json obj

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
 	$('.search-bar').keypress(function(e){
+
 		if(e.which == 13) {
 			var searchValue = $('.search-bar').val();
 			var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&gsrsearch=" + searchValue;
+
 			$.ajax({url: url,
 				dataType: "jsonp",
 	    	success: function(response){
+
 	        var obj = response['query']['pages'];
 	        for (var key in obj) {
 					  if (obj.hasOwnProperty(key)) {
-					    console.log(key);
+					    $('.link').append("http://en.wikipedia.com/?curid=" + key);
 					  }
 					}
+
+					for(var key in response.query.pages){
+    				if(response.query.pages.hasOwnProperty(key)){
+        			$('.title').append(response.query.pages[key].title);
+					  }
+					}
+
 	    	}
 			});
+
 		return false;
-		}//
+		}
 	});
 });
