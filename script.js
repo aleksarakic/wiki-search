4ecbef3ff9dcb72c43de6850588a0869445b753d
Aleksa <rakicaleksa@ymail.com>
2016-02-15 15:50:38 +0100

    search value extract

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	var arr = [];
 	$('.search-bar').keypress(function(e){
 		if(e.which == 13) {
-			var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&gsrsearch=" + "";
+			var searchValue = $('.search-bar').val();
+			var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&gsrsearch=" + searchValue;
 			$.ajax({url: url,
 				dataType: "jsonp",
 	    	success: function(response){
